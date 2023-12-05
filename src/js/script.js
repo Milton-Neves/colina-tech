46fbe3a576357662089d2329838a85477a52f697
milton.neto <milton.neto@parnamirim.rn.gov.br>
2023-12-05 14:22:10 -0300

    banner add

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+// Adicione scripts JavaScript conforme necessário
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Lógica de envio do formulário ou qualquer outra ação necessária
+    alert('Formulário enviado com sucesso!');
+});
